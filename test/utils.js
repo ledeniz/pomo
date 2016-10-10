5e10ae632dcb03d5089299756cbe918418aa1281
Tom Speak <TomSpeak@users.noreply.github.com>
2016-10-10 15:19:23 +0100

    Clear path for 3.0.0

--- test/utils.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import TestUtils from 'react-addons-test-utils'
-
-export default function renderShallow (component) {
-  const renderer = TestUtils.createRenderer()
-  renderer.render(component)
-  return renderer.getRenderOutput()
-}
