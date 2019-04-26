26ef0e3a1af320151469103db7dd20fe692a2360
deniz <github@ledeniz.de>
2019-04-27 00:56:06 +0200

    Update keybindings.jsx

--- assets/js/components/keybindings.jsx
+++ assets/js/components/keybindings.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 const Keybindings = ({  }) => {
   return (
     <div>
-      <span className="link-title">Keybindings</span>
+      <span className="link-title">keybindings</span>
       <div className="keybindings">
-        <div>Start / Pause — <kbd>Spacebar</kbd></div>
-        <div>Reset — <kbd>r</kbd></div>
+        <div>start / pause — <kbd>spacebar</kbd></div>
+        <div>reset — <kbd>r</kbd></div>
       </div>
     </div>
   )
